7f36e0c3af6bfcef47c837eb9d289b7ea2ce7cac
saksjain <saksjain@adobe.com>
2020-05-06 02:32:57 +0530

    deployed

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -17,7 +17,7 @@ import { IndexRouter } from "./controllers/v0/routes";
   app.use(bodyParser.json());
 
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   })
